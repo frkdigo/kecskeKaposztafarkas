881ac4402be25e30160610f37781c55879f75f97
frkdigo <furkonorbert16@gmail.com>
2022-03-29 14:46:01 +0200

    kép kattintás

--- script.js
+++ script.js
@@ -7,6 +7,23 @@ function $(elem){
     return document.querySelectorAll(elem);
 }
 
+var tomb = []; 
+
 function init(){
-    
+    $("footer p")[0].innerHTML = "Furkó Norbert";
+    for (let index = 0; index < 3; index++) {
+        $("#bal img")[index].addEventListener("click", kattintas);   
+    }
+}
+
+function kattintas(){
+    tomb.push(this.src);
+    console.log(tomb);
+}
+
+function felette(){
+    for (let index = 0; index < 3; index++) {
+        $("#bal img")[index].addEventListener("mouseover", felette);
+        $("#bal img")[index].addEventListener("mouseout", felette);
+    }
 }
