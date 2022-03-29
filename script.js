43a3a0e80e5e2c97bdab1a97fe6be36152e3de05
frkdigo <furkonorbert16@gmail.com>
2022-03-29 15:55:43 +0200

    veszélyes elemek nem lehetnek együtt, alert

--- script.js
+++ script.js
@@ -11,17 +11,25 @@ var tomb = [];
 
 function init(){
     $("footer p")[0].innerHTML = "Furkó Norbert";
+    $("footer p")[0].style.textAlign = "center";
+    $("footer p")[0].style.fontSize = "20px";
     for (let index = 0; index < 3; index++) {
         $("#bal img")[index].addEventListener("click", kattintas);   
         $("#bal img")[index].addEventListener("mouseover", felette);
         $("#bal img")[index].addEventListener("mouseout", alatta);
-
     }
 }
 
 function kattintas(){
-    tomb.push(this.src);
-    console.log(tomb);
+    var imgtag = `<img src='${event.target.src}' alt='${event.target.alt}' class='kicsi'>`;
+    tomb.push(event.target.alt); // lekérjük a képnek az src-jét.
+    console.log(tomb.indexOf("kecske"));
+    if(tomb.indexOf("kecske") >= 0 && tomb.indexOf("farkas") >= 0){
+        alert("Helytelen választás!");
+    }else{
+        ID("csonak").innerHTML += imgtag;
+        event.target.style.display = "none";
+    }
 }
 
 function felette(){
