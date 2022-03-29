71908213806d904c6fe17aeef30c506f9aefc6a5
frkdigo <furkonorbert16@gmail.com>
2022-03-29 14:09:24 +0200

    kiindulási alapok css-el

---
+++ script.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", init);
+
+function ID(elem){
+    return document.getElementById(elem);
+}
+function $(elem){
+    return document.querySelectorAll(elem);
+}
+
+function init(){
+    
+}
